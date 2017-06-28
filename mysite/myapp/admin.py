72b234f061dedc452f04fe5d3c9995f9f9581a61
Bruce <brucelau.email@gmail.com>
2017-06-28 14:19:37 -0400

    add topic in file

--- mysite/myapp/admin.py
+++ mysite/myapp/admin.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from  .models import Author, Book, Course, Student
+from  .models import Author, Book, Course, Student, Topic
 
 # Register your models here.
 admin.site.register(Author)
